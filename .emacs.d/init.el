2ad0fec86ab710c3a137bc674c173b1ec7504fba
Zeph Grunschlag <zgrunschlag@gmail.com>
2019-11-30 19:32:43 -0500

    emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,22 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (markdown-preview-mode flymake-yaml yaml-mode dockerfile-mode magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(ido-mode t)
+(setq ido-enable-flex-matching t) ; fuzzy matching is a must have
